97fc05e64d153cbb1d45a98ef051caf1692f3fe3
TheFakeMateo <skeetendomateo@gmail.com>
2021-07-14 00:58:06 -0500

    Add a few notes

---
+++ notes/notes.asm
@@ -0,0 +1,30 @@
+- 251 Pokemon (keep ghost and fossils)
+- Gen 2 party sprites
+- Version Based sprites
+- Shared SGB palettes
+- Shininess
+- Gender
+  - Make sure it is shown on the Nickname screen too
+- Proper breeding
+  - Alternately breeding like RPP3
+- Berry Trees on a step counter like RPP3
+- Apricorns and special pokeballs
+- Bug Catching Contest
+- Johto Safari Zone
+- Use HM moves by interacting with cut trees and such
+- Running Shoes and faster bike
+- Player Gender
+  - Hiro or Kris
+- Multiple bugfixes. Look at Shin Pokemon Red, pokered-clean, and red++ repos
+- A mixture of Gen 2 moves and custom moves added?
+- Movesets not identical to any generation
+- Move Deleter
+- Move Reminder
+- Try to mimic some Gen 2 UI things
+  - At least try to let you view attack info
+- Keep up with enemy pokemon move PP
+- Improve trainer AI
+- Add pockets to the bag
+- Remove all instances of IndexToPokedex and PokedexToIndex because everything is sorted the same now
+- Remove those routines to save space once the references are removed
+- Remove pointless junk data and messy code, see pokered-clean
