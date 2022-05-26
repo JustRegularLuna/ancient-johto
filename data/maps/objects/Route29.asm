0ce7cc82cd9356f91b7d56e050352792e690b8ea
Luna <skeetendomateo@gmail.com>
2022-05-26 11:55:35 -0500

    fix itemball

--- data/maps/objects/Route29.asm
+++ data/maps/objects/Route29.asm
@@ -6,7 +6,7 @@ Route29_Object:
 	def_signs
 
 	def_objects
-	object SPRITE_POKE_BALL, 49,  2, STAY, NONE, 1, POTION
+	object SPRITE_POKE_BALL, 48,  2, STAY, NONE, 1, POTION
 
 	def_warps_to ROUTE_29
 
