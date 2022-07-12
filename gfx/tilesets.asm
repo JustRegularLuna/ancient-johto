0cbf5b16acdda3307cded2c7137fc36b0cb604a5
Luna <skeetendomateo@gmail.com>
2022-07-12 09:59:21 -0500

    floating sections for tilesets

--- gfx/tilesets.asm
+++ gfx/tilesets.asm
@@ -1,31 +1,56 @@
-SECTION "Tilesets 1", ROMX
+SECTION "Overworld Tileset", ROMX
 
 Overworld_GFX::     INCBIN "gfx/tilesets/overworld.2bpp"
 Overworld_Block::   INCBIN "gfx/blocksets/overworld.bst"
 
+
+SECTION "House Tileset", ROMX
+
 House_GFX::         INCBIN "gfx/tilesets/house.2bpp"
 House_Block::       INCBIN "gfx/blocksets/house.bst"
+
+
+SECTION "Mansion Tileset", ROMX
+
 Mansion_GFX::       INCBIN "gfx/tilesets/mansion.2bpp"
 Mansion_Block::     INCBIN "gfx/blocksets/mansion.bst"
+
+
+SECTION "Ship Port Tileset", ROMX
+
 ShipPort_GFX::      INCBIN "gfx/tilesets/ship_port.2bpp"
 ShipPort_Block::    INCBIN "gfx/blocksets/ship_port.bst"
+
+
+SECTION "Interior Tileset", ROMX
+
 Interior_GFX::      INCBIN "gfx/tilesets/interior.2bpp"
 Interior_Block::    INCBIN "gfx/blocksets/interior.bst"
+
+
+SECTION "Plateau Tileset", ROMX
+
 Plateau_GFX::       INCBIN "gfx/tilesets/plateau.2bpp"
 Plateau_Block::     INCBIN "gfx/blocksets/plateau.bst"
 
 
-SECTION "Tilesets 2", ROMX
+SECTION "Gym Tileset", ROMX
 
 Dojo_GFX::
 Gym_GFX::           INCBIN "gfx/tilesets/gym.2bpp"
 Dojo_Block::
 Gym_Block::         INCBIN "gfx/blocksets/gym.bst"
 
+
+SECTION "Pokecenter Tileset", ROMX
+
 Mart_GFX::
 Pokecenter_GFX::    INCBIN "gfx/tilesets/pokecenter.2bpp"
 Mart_Block::
 Pokecenter_Block::  INCBIN "gfx/blocksets/pokecenter.bst"
+
+
+SECTION "Gate Tileset", ROMX
 
 ForestGate_GFX::
 Museum_GFX::
@@ -34,36 +59,74 @@ ForestGate_Block::
 Museum_Block::
 Gate_Block::        INCBIN "gfx/blocksets/gate.bst"
 
+
+SECTION "Forest Tileset", ROMX
+
 Forest_GFX::        INCBIN "gfx/tilesets/forest.2bpp"
 Forest_Block::      INCBIN "gfx/blocksets/forest.bst"
+
+
+SECTION "Facility Tileset", ROMX
+
 Facility_GFX::      INCBIN "gfx/tilesets/facility.2bpp"
 Facility_Block::    INCBIN "gfx/blocksets/facility.bst"
 
 
-SECTION "Tilesets 3", ROMX
+SECTION "Cemetery Tileset", ROMX
 
 Cemetery_GFX::      INCBIN "gfx/tilesets/cemetery.2bpp"
 Cemetery_Block::    INCBIN "gfx/blocksets/cemetery.bst"
+
+
+SECTION "Cavern Tileset", ROMX ; Kanto Caves
+
 Cavern_GFX::        INCBIN "gfx/tilesets/cavern.2bpp"
 Cavern_Block::      INCBIN "gfx/blocksets/cavern.bst"
+
+
+SECTION "Lobby Tileset", ROMX
+
 Lobby_GFX::         INCBIN "gfx/tilesets/lobby.2bpp"
 Lobby_Block::       INCBIN "gfx/blocksets/lobby.bst"
+
+
+SECTION "Ship Tileset", ROMX
+
 Ship_GFX::          INCBIN "gfx/tilesets/ship.2bpp"
 Ship_Block::        INCBIN "gfx/blocksets/ship.bst"
+
+
+SECTION "Lab Tileset", ROMX
+
 Lab_GFX::           INCBIN "gfx/tilesets/lab.2bpp"
 Lab_Block::         INCBIN "gfx/blocksets/lab.bst"
+
+
+SECTION "Club Tileset", ROMX
+
 Club_GFX::          INCBIN "gfx/tilesets/club.2bpp"
 Club_Block::        INCBIN "gfx/blocksets/club.bst"
+
+
+SECTION "Underground Tileset", ROMX
+
 Underground_GFX::   INCBIN "gfx/tilesets/underground.2bpp"
 Underground_Block:: INCBIN "gfx/blocksets/underground.bst"
 
 
-SECTION "Tilesets 4", ROMX
+SECTION "Johto Tileset", ROMX
 
 Johto_GFX::         INCBIN "gfx/tilesets/johto.2bpp"
 Johto_Block::       INCBIN "gfx/blocksets/johto.bst"
 
+
+SECTION "Players House Tileset", ROMX
+
 PlayersHouse_GFX::   INCBIN "gfx/tilesets/players_house.2bpp"
-PlayersRoom_GFX::    INCBIN "gfx/tilesets/players_room.2bpp"
 PlayersHouse_Block:: INCBIN "gfx/blocksets/players_house.bst"
+
+
+SECTION "Players Room Tileset", ROMX
+
+PlayersRoom_GFX::    INCBIN "gfx/tilesets/players_room.2bpp"
 PlayersRoom_Block::  INCBIN "gfx/blocksets/players_room.bst"
