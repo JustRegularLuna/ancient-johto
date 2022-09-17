14dc52a7303161e227111341a18c7bb568d98c6f
Luna <skeetendomateo@gmail.com>
2022-09-16 21:51:43 -0500

    No "Gen 1 Missing"

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -5351,6 +5351,12 @@ MoveHitTest:
 .doAccuracyCheck
 ; if the random number generated is greater than or equal to the scaled accuracy, the move misses
 ; note that this means that even the highest accuracy is still just a 255/256 chance, not 100%
+
+	; The following snippet is taken from Pokemon Crystal, it fixes the above bug.
+	ld a, b
+	cp $FF ; Is the value $FF?
+	ret z ; If so, we need not calculate, just so we can fix this bug.
+
 	call BattleRandom
 	cp b
 	jr nc, .moveMissed
