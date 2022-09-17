4b4f153a16949fa9f0149cb562d340fc922b3a64
Luna <skeetendomateo@gmail.com>
2022-09-16 23:13:15 -0500

    fix too many stars

--- engine/movie/splash.asm
+++ engine/movie/splash.asm
@@ -124,7 +124,7 @@ AnimateShootingStar:
 	ld a, [wMoveDownSmallStarsOAMCount]
 	cp 24
 	jr z, .next2
-	add 6 ; should be 4, but the extra 2 aren't visible on screen
+	add 4
 	ld [wMoveDownSmallStarsOAMCount], a
 .next2
 	call MoveDownSmallStars
