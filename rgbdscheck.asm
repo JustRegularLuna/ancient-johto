fc55f524ce8977c4788a2ddd07186a9c4ba37b7d
Luna <skeetendomateo@gmail.com>
2023-02-28 11:32:44 -0600

    Update rgbdscheck.asm

--- rgbdscheck.asm
+++ rgbdscheck.asm
@@ -1,14 +1,14 @@
-; pokered requires rgbds 0.4.2 or newer.
+; redgold currently uses rgbds 0.5.2 (builds in 0.6.0 but yells that every single macro is deprecated :roll_eyes:)
 MAJOR EQU 0
-MINOR EQU 4
+MINOR EQU 5
 PATCH EQU 2
 
 IF !DEF(__RGBDS_MAJOR__) || !DEF(__RGBDS_MINOR__) || !DEF(__RGBDS_PATCH__)
-	fail "pokered requires rgbds 0.4.2 or newer."
+	fail "redgold requires rgbds 0.5.2 or newer."
 ELSE
 IF (__RGBDS_MAJOR__ < MAJOR) || \
 	(__RGBDS_MAJOR__ == MAJOR && __RGBDS_MINOR__ < MINOR) || \
 	(__RGBDS_MAJOR__ == MAJOR && __RGBDS_MINOR__ == MINOR && __RGBDS_PATCH__ < PATCH)
-	fail "pokered requires rgbds 0.4.2 or newer."
+	fail "redgold requires rgbds 0.5.2 or newer."
 ENDC
 ENDC
